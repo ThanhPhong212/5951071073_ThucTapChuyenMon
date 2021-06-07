bb258580420356c696f58fceba1c41bfe8c7b0dd
ThanhPhong212 <bienthanhphong212@gmail.com>
2021-06-07 17:27:17 +0700

    update

--- QuanLyThuVien/QLThuVien.sql
+++ QuanLyThuVien/QLThuVien.sql
@@ -3,8 +3,6 @@ create database QLThuVien
 go
 use QLThuVien
 go
-
---drop table DOCGIA
 create table DOCGIA
 (
 	MaDocGia nchar(10),
@@ -14,7 +12,6 @@ create table DOCGIA
 	DiaChi nchar(100),
 	PRIMARY KEY (MaDocGia),
 )
---drop table SACH
 create table SACH
 (
 	MaSach nchar(10),
@@ -27,7 +24,6 @@ create table SACH
 	TinhTrang nchar(10),
 	PRIMARY KEY (MaSach),
 )
---drop table PHIEUMUON
 create table PHIEUMUON
 (
 	MaPhieu nchar(10),
@@ -37,7 +33,6 @@ create table PHIEUMUON
 	NgayPhaiTra datetime,
 	PRIMARY KEY (MaPhieu),
 )
---drop table PHIEUTRA
 create table PHIEUTRA
 (
 	MaPhieu nchar(10),
@@ -46,7 +41,6 @@ create table PHIEUTRA
 	NgayTra datetime,
 	PRIMARY KEY (MaPhieu),
 )
-
 create table ACCOUNT
 (
 	MaDocGia nchar(10) PRIMARY KEY,
@@ -54,7 +48,6 @@ create table ACCOUNT
 	MatKhau nchar(30),
 	Quyen int,
 )
-
 
 alter table PHIEUMUON add
 	constraint FK_PHIEUMUON_DOCGIA foreign key (MaDocGia) references DOCGIA (MaDocGia),
@@ -67,34 +60,33 @@ alter table PHIEUTRA add
 ALTER TABLE dbo.DOCGIA ADD
 	CONSTRAINT FK_DOCGIA_ACCOUNT FOREIGN KEY (MaDocGia) REFERENCES dbo.ACCOUNT(MaDocGia)
 
-
-
-insert into ACCOUNT values ('15','Nguyễn Hữu Hoàng Hiếu', '123456', 1)
-insert into ACCOUNT values ('1','phamhaingoc1', '123456', 1)
-insert into ACCOUNT values ('2','phamhaingoc2', '123456', 1)
-insert into ACCOUNT values ('3','phamminhhoang', '123456', 1)
-insert into ACCOUNT values ('7','nguyenthanhhai', '123456', 1)
-insert into ACCOUNT values ('999','admin', '1234', 0)
+insert into ACCOUNT values ('1','p1', '1234', 1)
+insert into ACCOUNT values ('2','p2', '1234', 1)
+insert into ACCOUNT values ('3','p3', '1234', 1)
+insert into ACCOUNT values ('4','p4', '1234', 1)
+insert into ACCOUNT values ('999','admin','', 0)
 ----
-insert into DOCGIA values ('1',N'Phạm Ngọc Hải','Nam',1996,N'510 Lý Thái Tổ')
-insert into DOCGIA values ('2',N'Phạm Hải NGọc','Nam',1994,N'20 Lý Thái Tổ')
-insert into DOCGIA values ('3',N'Phạm Minh Hoang','Nam',1994,N'20 Lý Thái Tổ')
-insert into DOCGIA values ('7',N'Nguyễn Thanh Hải',N'Nữ',1994,N'201 Lý Thái Tổ')
-insert into DOCGIA values ('15',N'Nguyễn Hữu Hoàng Hiếu',N'Nữ',1994,N'120 Lý Thái Tổ')
+insert into DOCGIA values ('1',N'Phạm Ngọc Hải','Nam',1999,N'Phú Yên')
+insert into DOCGIA values ('2',N'Phạm Hải NGọc','Nam',1996,N'Trà Vinh')
+insert into DOCGIA values ('3',N'Phạm Minh Hoang','Nam',2000,N'Bạc Liêu')
+insert into DOCGIA values ('4',N'Nguyễn Thanh Hải',N'Nữ',1994,N'Sóc Trăng')
 
 ----
-insert into SACH values ('1', N'Lập trình hướng đối tượng', N'Không biết', N'Lập trình', 'HCMUS', 1500, 3, N'Còn')
-insert into SACH values ('2', N'Nhập môn lập trình', N'Không biết', N'Lập trình', 'HCMUS', 1500, 10, N'Còn')
-insert into SACH values ('3', N'Kỹ Thuật Lập Trình', N'Không biết', N'Lập trình', 'HCMUS', 3000, 5, N'Còn')
-insert into SACH values ('4', N'Thiết Kế Phần Mềm', N'Không biết', N'Lập trình', 'HCMUS', 4000, 0, N'Hết')
+insert into SACH values ('1', N'Lập trình hướng đối tượng', N'Không biết', N'Lập trình', 'HCM', 1500, 3, N'Còn')
+insert into SACH values ('2', N'Nhập môn lập trình', N'Không biết', N'Lập trình', 'HCM', 1500, 10, N'Còn')
+insert into SACH values ('3', N'Kỹ Thuật Lập Trình', N'Không biết', N'Lập trình', 'HCM', 3000, 5, N'Còn')
+insert into SACH values ('4', N'Thiết Kế Phần Mềm', N'Không biết', N'Lập trình', 'C#', 4000, 0, N'Hết')
+insert into SACH values ('5', N'Thiết Kế Đồ Họa', N'Không biết', N'Thiết Kế', 'UTC2', 4000, 0, N'Hết')
+insert into SACH values ('6', N'Xây Dựng', N'Không biết', N'Lập trình', 'HCM', 4000, 6, N'Còn')
+insert into SACH values ('7', N'Nghìn lẻ một đêm', N'Không biết', N'Truyện ngắn', 'Kim Đồng', 4000, 7, N'Còn')
+insert into SACH values ('8', N'Thánh hài', N'Không biết', N'Hài', 'Kim Đồng', 4000, 0, N'Hết')
 ---
-insert into PHIEUMUON values ('1','1','1','1/1/2017', '1/2/2017')
-insert into PHIEUMUON values ('2','2','1','1/2/2017', '1/3/2017')
-insert into PHIEUMUON values ('3','2','2','1/3/2017', '1/4/2017')
+insert into PHIEUMUON values ('1','1','1','5/5/2021', '7/6/2021')
+insert into PHIEUMUON values ('2','2','1','7/5/2021', '7/6/2021')
+insert into PHIEUMUON values ('3','2','2','7/5/2021', '7/6/2021')
 
 ---
-insert into PHIEUTRA values ('1','1','2','1/1/2016')
-insert into PHIEUTRA values ('2','2','1','1/2/2016')
-insert into PHIEUTRA values ('3','2','2','1/3/2016')
+insert into PHIEUTRA values ('1','1','2','7/6/2021')
+insert into PHIEUTRA values ('2','2','1','7/6/2021')
+insert into PHIEUTRA values ('3','2','2','7/6/2021')
 go
-select Docgia.MaDocGia, TenDangNhap, HoTen, GioiTinh, NamSinh, DiaChi fROM DOCGIA,ACCOUNT WHERE DOCGIA.MaDocGia=ACCOUNT.MaDocGia 
